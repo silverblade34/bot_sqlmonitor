1047b8af517717964f6e93f179046a8646913f5a
Marcos Pacheco Tacay <90942815+silverblade34@users.noreply.github.com>
2023-08-11 17:14:03 -0500

    Update connection.py

--- src/sql/connection.py
+++ src/sql/connection.py
@@ -3,10 +3,10 @@ import pyodbc
 class ConnectionSQL:
     def __init__(self):
         # Establecer la conexión a la base de datos
-        server = '34.225.53.169'
-        database = 'DEV_Eventos'
-        username = 'sa'
-        password = '0313334'
+        server = 'IR SERVER'
+        database = 'DATEBASE'
+        username = 'USERNAME'
+        password = 'PASSWORD'
         cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
         # Crear un cursor para ejecutar comandos SQL
         self.cursor = cnxn.cursor()
